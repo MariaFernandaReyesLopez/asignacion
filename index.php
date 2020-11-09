3674d4836f1d456d23ad358980eafe8c417f0c4a
MariaFernandaReyesLopez <1718110411@utectulancingo.edu.mx>
2020-11-08 19:37:53 -0600

    Login creado y terminado

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="https://lh3.googleusercontent.com/proxy/y_cVPFlbIfhnqU4NQafT6kmuXLxqcXAJHRg764aoC8YTrpJt98svsC7JVF2QZJtdrxEPWvv_7eA14hDK3wzG7g">
+    <title>Sistema de asignacion de horarios y materias - UTec Tulancingo</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+    <div class="login-box">
+      <img src="images/logo_actual.png" class="avatar" alt="Avatar Image">
+      <h1>INGRESAR</h1>
+      <form>
+        <!-- USERNAME INPUT -->
+        <label for="username">Usuario</label>
+        <input type="text" placeholder="Escribe tu usuario">
+        <!-- PASSWORD INPUT -->
+        <label for="password">Contraseña</label>
+        <input type="password" placeholder="Escribe la contraseña">
+        <input type="submit" value="Entrar">
+      </form>
+    </div>
+  </body>
+</html>
